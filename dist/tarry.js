4cb87b1729f89b16b3cb842bb9a58e44c0e4acd6
estrattonbailey <ericstrattonbailey@gmail.com>
2016-10-04 11:47:26 -0400

    Add override capability, 0ms delay, ES5 version

--- dist/tarry.js
+++ dist/tarry.js
@@ -1,5 +1,5 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.tarry = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -9,17 +9,18 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 var run = function run(cb, args) {
   cb();
-  args.length > 0 ? args.shift().apply(undefined, _toConsumableArray(args)) : null;
+  args.length > 0 && args.shift().apply(undefined, _toConsumableArray(args));
 };
 
 var tarry = exports.tarry = function tarry(cb) {
-  var delay = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+  var delay = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
   return function () {
     for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return delay ? setTimeout(function () {
+    var override = 'number' === typeof args[0] ? args[0] : null;
+    return 'number' === typeof override && override > -1 ? tarry(cb, override) : 'number' === typeof delay && delay > -1 ? setTimeout(function () {
       return run(cb, args);
     }, delay) : run(cb, args);
   };
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const run = (cb, args) => {
   cb()
-  args.length > 0 ? args.shift()(...args) : null
+  args.length > 0 && args.shift()(...args)
 }
 
-export const tarry = (cb, delay = false) => (...args) => (
-  delay ? setTimeout(() => run(cb, args), delay) : run(cb, args)
-)
+export const tarry = (cb, delay = null) => (...args) => {
+  let override = 'number' === typeof args[0] ? args[0] : null 
+  return 'number' === typeof override && override > -1 
+    ? tarry(cb, override) 
+    : 'number' === typeof delay && delay > -1 
+      ? setTimeout(() => run(cb, args), delay) 
+      : run(cb, args)
+}
 
 export const queue = (...args) => () => args.shift()(...args)
