5ac78170630517ac63e01109c07ca78f3d949e8d
estrattonbailey <ericstrattonbailey@gmail.com>
2016-10-03 22:37:40 -0400

    Add some tests, point main package.json to src

--- test/index.js
+++ test/index.js
@@ -1,1 +1,56 @@
 import test from 'ava'
+import sequence, { tarry, queue } from '../dist/tarry.js'
+
+test.cb('tarry (internal) fires delayed function', t => {
+  t.plan(1)
+
+  let res = ''
+
+  const d = tarry(() => {
+    res = 'Hello world' 
+  }, 500)
+
+  d(() => {
+    t.is(res, 'Hello world')
+    t.end()
+  })
+})
+
+test.cb('queue fires sequence', t => {
+  t.plan(1)
+
+  let res = ''
+
+  const one = tarry(() => {
+    res = 'Hello' 
+  }, 500)
+
+  const two = tarry(() => {
+    res += ' world'
+    console.log(res)
+  })
+
+  queue(one, two, tarry(() => {
+    t.is(res, 'Hello world')
+    t.end()
+  }))()
+})
+
+test.cb('default returns sequencer', t => {
+  t.plan(2)
+
+  let res = ''
+
+  const seq = sequence().then(
+    () => { res = 'Hello' }, 500
+  ).then(
+    () => { res += ' world' }, 500
+  )
+
+  t.deepEqual(seq, Object())
+
+  seq.then(() => {
+    t.is(res, 'Hello world')
+    t.end()
+  }).done()
+})
