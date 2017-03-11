fb7c82632dafb25221216744ada796ef32f66df1
estrattonbailey <ericstrattonbailey@gmail.com>
2017-03-11 10:55:54 -0500

    Monorepo, fix test

--- package/test/index.js
+++ package/test/index.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import sequence, { tarry, queue } from '../dist/tarry.js'
+import { tarry, queue } from '../'
 
 test.cb('tarry (internal) fires delayed function', t => {
   t.plan(1)
@@ -27,30 +27,10 @@ test.cb('queue fires sequence', t => {
 
   const two = tarry(() => {
     res += ' world'
-    console.log(res)
   })
 
   queue(one, two, tarry(() => {
     t.is(res, 'Hello world')
     t.end()
   }))()
-})
-
-test.cb('default returns sequencer', t => {
-  t.plan(2)
-
-  let res = ''
-
-  const seq = sequence().then(
-    () => { res = 'Hello' }, 500
-  ).then(
-    () => { res += ' world' }, 500
-  )
-
-  t.deepEqual(seq, Object())
-
-  seq.then(() => {
-    t.is(res, 'Hello world')
-    t.end()
-  }).done()
 })
